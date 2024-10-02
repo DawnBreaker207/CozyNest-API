63ef6d25e697b151bbc169be16e98c3910ff82ed
DawnBreaker207 <tunganhngo207@gmail.com>
2024-10-02 22:35:13 +0700

    fix: fix valid body

--- src/routes/vnpay.router.ts
+++ src/routes/vnpay.router.ts
@@ -1,11 +1,14 @@
-import { vnpayCreate, vnpayIpn, vnpayReturn } from "@/controllers/payment.controller";
-import { Router } from "express";
-
+import {
+  vnpayCreate,
+  vnpayIpn,
+  vnpayReturn,
+} from '@/controllers/payment.controller';
+import { Router } from 'express';
 
 const routeVnpay = Router();
 
-routeVnpay.post('/create-vnpay',vnpayCreate);
-routeVnpay.get('/vnpay-ipn',vnpayIpn);
-routeVnpay.get('/vnpay-return',vnpayReturn);
+routeVnpay.post('/create-vnpay', vnpayCreate);
+routeVnpay.get('/vnpay-ipn', vnpayIpn);
+routeVnpay.get('/vnpay-return', vnpayReturn);
 
 export default routeVnpay;
