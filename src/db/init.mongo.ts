365138424ca084b4dc64763df43373f47d77e6c9
Ngo Tung Anh <126256917+DawnBreaker207@users.noreply.github.com>
2024-10-16 14:47:31 +0700

    feat: update db config

--- src/db/init.mongo.ts
+++ src/db/init.mongo.ts
@@ -23,13 +23,29 @@ class Database {
   }
 
   private connect() {
+    mongoose.connection.on('connecting', () => {
+      console.log('MongoDB is connecting...');
+    });
+
+    mongoose.connection.on('connected', () => {
+      console.log('MongoDB connected successfully');
+    });
+
+    mongoose.connection.on('disconnecting', () => {
+      console.log('MongoDB is disconnecting...');
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('MongoDB disconnected');
+    });
+
     mongoose
       .connect(URI || '')
-      .then((_) => {
-        console.log(`Connect DB success`);
+      .then(() => {
+        console.log(`Connect mongoDB success`);
         // return seedData();
       })
-      .catch((err) => console.log(`Error Connect ${err}`));
+      .catch((err) => console.log(`Error connecting to MongoDB: ${err}`));
   }
 
   public static getInstance() {
