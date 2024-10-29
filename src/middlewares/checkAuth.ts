fe8e8d64d54441933ad02f9fc45cd804e5fbe7b0
Ngo Tung Anh <126256917+DawnBreaker207@users.noreply.github.com>
2024-10-29 10:11:34 +0700

    feat: update check expired token middleware

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,9 +1,9 @@
+import { JwtPayload } from '@/@types/jsonwebtoken';
 import { messagesError } from '@/constants/messages';
 import { UserType } from '@/interfaces/User';
 import User from '@/models/User';
-import { SECRET_ACCESS_TOKEN } from '@/utils/env';
 import { AppError } from '@/utils/errorHandle';
-import { verifyToken } from '@/utils/jwt';
+import { checkExpiredToken, decodedToken, verifyToken } from '@/utils/jwt';
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
@@ -16,21 +16,23 @@ export const checkAuth: RequestHandler = async (req, res, next) => {
         message: messagesError.TOKEN_INVALID,
       });
     }
+    // Check token expired
 
     // Check token valid
-    const decode = verifyToken(token) as { _id?: string };
+    const decode = verifyToken(token);
 
     if (!decode) {
       return res.status(StatusCodes.BAD_REQUEST).json({
         message: messagesError.TOKEN_INVALID,
       });
     }
+
     // Check user valid
     const user: UserType | null = await User.findById(decode._id);
     if (!user) {
       throw new AppError(StatusCodes.NOT_FOUND, 'User not exist');
     }
-    req.user = user;
+
     next();
   } catch (error) {
     next(error);
