162cb0f7fbf4954bac119a6060fc4212ce3e381e
phucnv050004 <phucvan050004@gmail.com>
2024-11-02 00:35:09 +0700

    sendOrder

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -3,15 +3,36 @@ import { messagesSuccess } from '@/constants/messages';
 import { sendExportMail } from '@/utils/texts';
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import pug from 'pug';
 
 const sendMailRequest: RequestHandler = async (req, res, next) => {
   try {
+    const { email, orderId, orderDate, customerName, customerPhone, customerAddress, products, totalAmount, paymentMethod, paymentStatus } = req.body;
+
+    // Render email từ template pug với dữ liệu đơn hàng
+    const emailContent = pug.renderFile('path/to/invoiceTemplate.pug', {
+      customerName,
+      orderDate,
+      orderId,
+      customerPhone,
+      customerAddress,
+      products,
+      totalAmount,
+      paymentMethod,
+      paymentStatus,
+    });
+
+    // Thiết lập thông tin email
     const emailOption = {
-      email: req.body.email,
-      subject: req.body.subject,
-      text: sendExportMail(req.body.subject, req.body.content),
+      email: email as string,  // Ensure email is a string
+      subject: `Hóa đơn cho đơn hàng ${orderId}`,
+      text: emailContent,  // Rename html to text
     };
+
+    // Gửi email
     await configSendMail(emailOption);
+
+    // Phản hồi thành công
     return res.status(StatusCodes.OK).json({
       message: messagesSuccess.SEND_EMAIL_SUCCESS,
     });
@@ -19,5 +40,6 @@ const sendMailRequest: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
+
 
 export { sendMailRequest };
