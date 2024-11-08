cbb0c0b0ac3cc089207d906da84103ed345649b2
phucnv050004 <phucvan050004@gmail.com>
2024-11-08 15:54:28 +0700

    middlewares

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -1,23 +1,42 @@
-import { createArticle, deleteArticle, getAll, getArticleDetail, updateArticle } from "@/controllers/article.controller";
-import validBodyRequest from "@/middlewares/validBodyRequest";
-import { articleSchema } from "@/validations/article.validation";
-import { Router } from "express";
-
+import {
+  createArticle,
+  deleteArticle,
+  getAll,
+  getArticleDetail,
+  updateArticle,
+} from '@/controllers/article.controller';
+import { checkAuth } from '@/middlewares/checkAuth';
+import { checkPermission } from '@/middlewares/checkPermission';
+import validBodyRequest from '@/middlewares/validBodyRequest';
+import { articleSchema } from '@/validations/article.validation';
+import { Router } from 'express';
 
 const routeArticle = Router();
 //*get all
-routeArticle.get('/',getAll);
+routeArticle.get('/', getAll);
 
 //*get one
-routeArticle.get('/:id',getArticleDetail);
+routeArticle.get('/:id', getArticleDetail);
 
 //*create
-routeArticle.post('/',createArticle,validBodyRequest(articleSchema));
+routeArticle.post(
+  '/',
+  createArticle,
+  validBodyRequest(articleSchema),
+  checkAuth,
+  checkPermission,
+);
 
 //*update
-routeArticle.put('/:id',updateArticle,validBodyRequest(articleSchema));
+routeArticle.put(
+  '/:id',
+  updateArticle,
+  validBodyRequest(articleSchema),
+  checkAuth,
+  checkPermission,
+);
 
 //*delete
-routeArticle.delete('/:id',deleteArticle);
+routeArticle.delete('/:id', deleteArticle, checkAuth, checkPermission);
 
 export default routeArticle;
