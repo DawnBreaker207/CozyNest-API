cd91d91ead19a572db69e740aafac7442c04a6a0
Ngo Tung Anh <126256917+DawnBreaker207@users.noreply.github.com>
2024-11-18 12:51:13 +0700

    feat: update checkt auth
    - Check access token from cookie
    - If access expired, check refresh exist
    - If refresh exist, create access token from refresh

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,38 +1,65 @@
 import { messagesError } from '@/constants/messages';
 import { UserType } from '@/interfaces/User';
 import User from '@/models/User';
+import { SECRET_ACCESS_TOKEN } from '@/utils/env';
 import { AppError } from '@/utils/errorHandle';
-import { verifyToken } from '@/utils/jwt';
+import { createToken, decodedToken, verifyToken } from '@/utils/jwt';
 import logger from '@/utils/logger';
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 export const checkAuth: RequestHandler = async (req, res, next) => {
   try {
-    // Check token exist
-// TODO: update feature
+    // TODO: update feature
     // const token = req.headers?.authorization?.split(' ')[1];
-    const token = req.cookies.refreshToken;
-    if (!token) {
+
+    //* Check access and refresh token exist
+    const { accessToken, refreshToken } = req.cookies;
+    if (!accessToken || !refreshToken) {
       logger.log('error', 'Check auth error: Token not exist');
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        message: messagesError.TOKEN_INVALID,
-      });
+      //   return res.status(StatusCodes.BAD_REQUEST).json({
+      //     message: messagesError.TOKEN_INVALID,
+      // });
     }
-    // Check token expired
 
-    // Check token valid
-    const decode = verifyToken(token);
-
+    //* Check token expired
+    const decode = verifyToken(accessToken);
     if (!decode) {
       logger.log('error', 'Check auth error: Token invalid');
+      // return res.status(StatusCodes.BAD_REQUEST).json({
+      //   message: messagesError.TOKEN_INVALID,
+      // });
+    }
+
+    //* If access expired, check refresh token
+    const checkRefresh = verifyToken(refreshToken);
+    if (!checkRefresh) {
+      logger.log('error', 'Check auth error: Refresh token invalid or expired');
       return res.status(StatusCodes.BAD_REQUEST).json({
         message: messagesError.TOKEN_INVALID,
       });
     }
 
-    // Check user valid
-    const user: UserType | null = await User.findById(decode._id);
+    //* If refresh exist, create access from refresh and send to cookie
+    const refreshDecoded = await decodedToken(refreshToken);
+    const newAccess = createToken(
+      { sub: refreshDecoded.sub, ...refreshDecoded },
+      SECRET_ACCESS_TOKEN as string,
+    );
+    if (!newAccess) {
+      throw new AppError(
+        StatusCodes.BAD_REQUEST,
+        'Error when create access token',
+      );
+    }
+    res.cookie('accessToken', newAccess, {
+      httpOnly: true,
+    });
+
+    //* Check user exist, decoded token from access
+    const decodeAccess = verifyToken(newAccess, SECRET_ACCESS_TOKEN);
+
+    const user: UserType | null = await User.findById(decodeAccess.payload._id);
     if (!user) {
       logger.log('error', 'Check auth error: User not exist');
       throw new AppError(StatusCodes.NOT_FOUND, 'User not exist');
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import jwt from 'jsonwebtoken';
 import { JWT, SECRET_ACCESS_TOKEN, SECRET_REFRESH_TOKEN } from './env';
 import { AppError } from './errorHandle';
+import logger from './logger';
 /**
  *
  * @param payload
@@ -10,52 +11,65 @@ import { AppError } from './errorHandle';
  * @returns
  */
 const createToken = (
-    payload: Object,
-    SECRET_CODE: string,
-    expiresIn = '5m',
-  ) => {
-    const token = jwt.sign(payload, SECRET_CODE, {
-      expiresIn,
-    });
-    return token;
-  },
-  /**
-   *
-   * @param token
-   * @param SECRET_CODE
-   * @param options
-   * @returns
-   */
-  verifyToken = (
-    token: string,
-    SECRET_CODE: string = SECRET_ACCESS_TOKEN ||
-      JWT ||
-      (SECRET_REFRESH_TOKEN as string),
-    options?: jwt.SignOptions | undefined,
-  ) => <jwt.JwtPayload>jwt.verify(token, SECRET_CODE, options),
-  // Decode token and take payload
-  decodedToken = async (token: string) => {
-    const decoded = <jwt.Jwt>jwt.decode(token, { complete: true });
-    return decoded as jwt.JwtPayload;
-  },
-  checkExpiredToken = async (token: string) => {
-    // Decode token and take payload
-    const checkToken = await decodedToken(token);
-    if (!checkToken) {
-      throw new AppError(StatusCodes.BAD_REQUEST, 'Invalid token');
-    }
+  payload: Object,
+  SECRET_CODE: string,
+  expiresIn = '5m',
+) => {
+  const token = jwt.sign(payload, SECRET_CODE, {
+    expiresIn,
+  });
+  if (!token) {
+    logger.log('error', `Create token error `);
+    throw new AppError(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      'Error in create token',
+    );
+  }
+  return token;
+};
+/**
+ *
+ * @param token
+ * @param SECRET_CODE
+ * @param options
+ * @returns
+ */
+const verifyToken = (
+  token: string,
+  SECRET_CODE: string = SECRET_ACCESS_TOKEN ||
+    JWT ||
+    (SECRET_REFRESH_TOKEN as string),
+  options?: jwt.SignOptions | undefined,
+) => <jwt.JwtPayload>jwt.verify(token, SECRET_CODE, options);
 
-    const tokenTimes = new Date((checkToken.exp as number) * 1000),
-      now = new Date();
-    // Check token expired
-    if (tokenTimes < now) {
-      // TODO: check if have refresh token, return new access token
-      return createToken(
-        checkToken.sub as string,
-        SECRET_ACCESS_TOKEN as string,
-      );
-    }
-    // if (tokenTimes > now) {
-    // }
-  };
+// Decode token and take payload
+const decodedToken = async (token: string) => {
+  const decoded = <jwt.Jwt>jwt.decode(token, { complete: true });
+  if (!decoded) {
+    logger.log('error', 'Verify token failed or error');
+    throw new AppError(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      'Error in verify token',
+    );
+  }
+  return decoded as jwt.JwtPayload;
+};
+
+const checkExpiredToken = async (token: string) => {
+  // Decode token and take payload
+  const checkToken = await decodedToken(token);
+  if (!checkToken) {
+    throw new AppError(StatusCodes.BAD_REQUEST, 'Invalid token');
+  }
+
+  const tokenTimes = new Date((checkToken.exp as number) * 1000),
+    now = new Date();
+  // Check token expired
+  if (tokenTimes < now) {
+    // TODO: check if have refresh token, return new access token
+    return createToken(checkToken.sub as string, SECRET_ACCESS_TOKEN as string);
+  }
+  // if (tokenTimes > now) {
+  // }
+};
 export { checkExpiredToken, createToken, decodedToken, verifyToken };
