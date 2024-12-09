7da578c20843fdf47dc735c7173be4bc19eabd2e
phucnv050004 <phucvan050004@gmail.com>
2024-12-09 16:02:58 +0700

    fix article

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -29,7 +29,7 @@ routeArticle.post(
   '/',
   // checkAuth,
   // checkPermission,
-  validBodyRequest(articleSchema),
+  // validBodyRequest(articleSchema),
   // #swagger.tags = ['Articles']
   createArticle,
 );
